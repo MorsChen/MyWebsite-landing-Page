76a60c22263245c2af1e91aab02ef5cc21061d43
MorsChen <52906439+MorsChen@users.noreply.github.com>
2019-07-23 23:22:10 +0700

    new versiom

--- MasterJS/javascripts.js
+++ MasterJS/javascripts.js
@@ -8,12 +8,29 @@ function formatCurrency(type,value) {
     return formatter.format(value);
 }
 
-function updateResults(from, to, conversionRate) {
-    const value = document.getElementById("inputValue").value;
-    const convertedValue = conversionRate * value;
-    const formattedCurrency = formatCurrency(to.toUpperCase(), convertedValue);
+// function USDtoVND() {
+//     const inputConValue = document.getElementById("inputvalue").value;
+//     const conValue = inputConValue * exchangeRateUS;
+//     const conValueVND = conValue;
+//     const formatConValueVND = formatCurrency("vnd",conValueVND);
+//     console.log("convert: ",formatConValueVND);
+//     document.getElementById("convertedValue").value = formatConValueVND;
+// }
+
+function USDtoVND() {
+    const inputValue = document.getElementById("inputValue").value ;
+    const convertValue = inputValue * exchangeRateUSD ;
+    const convertVND = convertValue;
+    const formattedCurrency = formatCurrency("vnd", convertVND);
+    console.log("convert: ", formattedCurrency)
     document.getElementById("convertedValue").innerHTML = formattedCurrency;
 }
+
+// function USDtoVND() {
+//     if {
+        
+//     }
+// }
 
 // function updateResults(from, to, conversionRate) {
 //     const value = document.getElementById("inputValue").value;
@@ -22,31 +39,7 @@ function updateResults(from, to, conversionRate) {
 //     document.getElementById("convertedValue").innerHTML = formattedCurrency;
 //   }
 
-function getConversion(from, to) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(
-        "GET",
-        `https://free.currconv.com/api/v7/convert?q=${from}_${to}&compact=ultra&apiKey=9a02420c4a5e378f2215`
-    );
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText);
-            const conversionRate = response[`${from}_${to}`];
-            updateResults(from, to, conversionRate);
-        } else {
-            // console.log("Failed request!");
-            alert("Request failed. Returned status of " + xhr.status);
-        }
-    };
-    xhr.send();
-}
 
-function test() {
-    console.log("updating the input... coule we do more?");
-}
-
-
-  
 // function getConversion(from, to) {
 //     var xhr = new XMLHttpRequest();
 //     xhr.open(
@@ -64,3 +57,10 @@ function test() {
 //     };
 //     xhr.send();
 //   }
+
+// function test() {
+//     console.log("updating the input... coule we do more?");
+// }
+
+
+  
